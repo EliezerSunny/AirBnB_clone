a4a7c6e1159e75ad79cafab49396f55f90d455ee
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-18 17:20:37 +0000

    modified

---
+++ models/id.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent a User id.
+
+    Attributes:
+        id (str): The id.
+    """
+
+    id = ""
