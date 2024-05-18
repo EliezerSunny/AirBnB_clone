626b6974eea82cea2c397b87458719fcd4542cea
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-18 16:52:15 +0000

    modified

--- console.py
+++ console.py
@@ -1,133 +1,54 @@
 #!/usr/bin/python3
-"""  AirBnB Console """
-import cmd
-import sys
-import json
-import os
-from models import storage
+"""
+Module for AirBnB_clone project
+"""
+
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+import uuid
+from datetime import datetime
 
 
-class HBNBCommand(cmd.Cmd):
-    """ General Class for HBNBCommand """
-    prompt = '(hbnb) '
-    classes = {'BaseModel': BaseModel, 'User': User, 'City': City,
-               'Place': Place, 'Amenity': Amenity, 'Review': Review,
-               'State': State}
+class User(BaseModel):
+    """
+    Class representing a user in the AirBnB_clone project.
+    """
 
-    def do_quit(self, arg):
-        """ Exit method for quit typing """
-        exit()
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new user.
 
-    def do_EOF(self, arg):
-        """ Exit method for EOF """
-        print('')
-        exit()
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
 
-    def emptyline(self):
-        """ Method to pass when emptyline entered """
-        pass
+    def __str__(self):
+        """
+        Return a string representation of the user.
+        """
+        return "[User] ({}) {}".format(self.id, self.__dict__)
 
-    def do_create(self, arg):
-        """ Create a new instance """
-        if len(arg) == 0:
-            print('** class name missing **')
-            return
-        new = None
-        if arg:
-            arg_list = arg.split()
-            if len(arg_list) == 1:
-                if arg in self.classes.keys():
-                    new = self.classes[arg]()
-                    new.save()
-                    print(new.id)
-                else:
-                    print("** class doesn't exist **")
+    def save(self):
+        """
+        Update the updated_at attribute and save the user.
+        """
+        self.updated_at = datetime.now()
+        storage.save(self)
 
-    def do_show(self, arg):
-        """ Method to print instance """
-        if len(arg) == 0:
-            print('** class name missing **')
-            return
-        elif arg.split()[0] not in self.classes:
-            print("** class doesn't exist **")
-            return
-        elif len(arg.split()) > 1:
-            key = arg.split()[0] + '.' + arg.split()[1]
-            if key in storage.all():
-                i = storage.all()
-                print(i[key])
-            else:
-                print('** no instance found **')
-        else:
-            print('** instance id missing **')
-
-    def do_destroy(self, arg):
-        """ Method to delete instance with class and id """
-        if len(arg) == 0:
-            print("** class name missing **")
-            return
-        arg_list = arg.split()
-        try:
-            obj = eval(arg_list[0])
-        except Exception:
-            print("** class doesn't exist **")
-            return
-        if len(arg_list) == 1:
-            print('** instance id missing **')
-            return
-        if len(arg_list) > 1:
-            key = arg_list[0] + '.' + arg_list[1]
-            if key in storage.all():
-                storage.all().pop(key)
-                storage.save()
-            else:
-                print('** no instance found **')
-                return
-
-    def do_all(self, arg):
-        """ Method to print all instances """
-        if len(arg) == 0:
-            print([str(a) for a in storage.all().values()])
-        elif arg not in self.classes:
-            print("** class doesn't exist **")
-        else:
-            print([str(a) for b, a in storage.all().items() if arg in b])
-
-    def do_update(self, arg):
-        """ Method to update JSON file"""
-        arg = arg.split()
-        if len(arg) == 0:
-            print('** class name missing **')
-            return
-        elif arg[0] not in self.classes:
-            print("** class doesn't exist **")
-            return
-        elif len(arg) == 1:
-            print('** instance id missing **')
-            return
-        else:
-            key = arg[0] + '.' + arg[1]
-            if key in storage.all():
-                if len(arg) > 2:
-                    if len(arg) == 3:
-                        print('** value missing **')
-                    else:
-                        setattr(
-                            storage.all()[key],
-                            arg[2],
-                            arg[3][1:-1])
-                        storage.all()[key].save()
-                else:
-                    print('** attribute name missing **')
-            else:
-                print('** no instance found **')
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    def to_dict(self):
+        """
+        Return a dictionary containing all keys/values of the instance.
+        """
+        user_dict = self.__dict__.copy()
+        user_dict["__class__"] = self.__class__.__name__
+        user_dict["created_at"] = self.created_at.isoformat()
+        user_dict["updated_at"] = self.updated_at.isoformat()
+        return user_dict
